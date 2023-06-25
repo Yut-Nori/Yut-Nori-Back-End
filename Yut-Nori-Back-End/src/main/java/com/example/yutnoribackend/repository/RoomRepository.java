c1576986b7261c08df429d8b502b00141e88ff37
jinsan <lazybones1@naver.com>
2023-06-25 15:06:02 +0900

    feat(repository 추가)

---
+++ Yut-Nori-Back-End/src/main/java/com/example/yutnoribackend/repository/RoomRepository.java
@@ -0,0 +1,7 @@
+package com.example.yutnoribackend.repository;
+
+import com.example.yutnoribackend.entity.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomRepository extends JpaRepository<Room, Integer> {
+}
