5fa42aa0ebbed08a1bbf4732ed70d11dc5612b3e
jinsan <lazybones1@naver.com>
2023-07-01 12:02:35 +0900

    feat(custom exception : DataNotFoundException)
    delete시 데이터 찾지 못한경우 발생하는 exception 추가

---
+++ Yut-Nori-Back-End/src/main/java/com/example/yutnoribackend/exception/DataNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.yutnoribackend.exception;
+
+public class DataNotFoundException extends Exception{
+    public DataNotFoundException(String message){
+        super(message);
+    }
+}
